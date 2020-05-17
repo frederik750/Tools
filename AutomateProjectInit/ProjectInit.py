2925bcd52d6105535ce24813b8bfdc6178306587
frederik750 <frede_rind@hotmail.dk>
2020-05-17 22:27:13 +0200

    bash command create folder

---
+++ AutomateProjectInit/ProjectInit.py
@@ -0,0 +1,16 @@
+#Automate git init process while making directory
+import os
+import sys
+
+path = "/mnt/c/Users/frede/Documents/GitHub/"
+
+def init():
+    directoryName = str(sys.argv[1])
+    try:
+        os.makedirs(path + directoryName)
+    except FileExistsError:
+        print("This folder already exits")
+
+
+if __name__ == "__main__":
+    init()
---
+++ AutomateProjectInit/ProjectInit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+function init(){
+    cd /mnt/c/Users/frede/Documents/GitHub/
+    python3 /mnt/c/Users/frede/Documents/GitHub/Tools/AutomateProjectInit/ProjectInit.py $1 #Path to script to run
+}
