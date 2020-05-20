e968d752c414ecee89f2f80445f20bffae112fa5
frederik750 <frede_rind@hotmail.dk>
2020-05-20 18:02:23 +0200

    added git clone

--- AutomateProjectMod/ProjectInit.py
+++ AutomateProjectMod/ProjectInit.py
@@ -4,20 +4,22 @@ import sys
 import datetime
 from Credentials import token
 from github import Github
-
+from git import Repo
 
 path = "/mnt/c/Users/frede/Documents/GitHub/"
 
 def init():
     directoryName = str(sys.argv[1])
+    git_repo = "https://github.com/frederik750/" + directoryName + ".git"
     try:
         os.makedirs(path + directoryName)
         g = Github(token)
         user = g.get_user()
-        user.create_repo(directoryName)
+        repo = user.create_repo(directoryName, auto_init=True)
+        Repo.clone_from(git_repo, path + directoryName)
+        print("Directory {Directory} was created at {path} {time}".format(Directory = directoryName, path = path, time = str(datetime.datetime.now())))
     except FileExistsError:
         print("This folder already exits")
-    print("Directory {Directory} was created at {path} {time}".format(Directory = directoryName, path = path, time = str(datetime.datetime.now())))
 
 if __name__ == "__main__":
     init()
--- AutomateProjectMod/ProjectRm.py
+++ AutomateProjectMod/ProjectRm.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import datetime
+from github import Github
+from Credentials import token
 
 path = "/mnt/c/Users/frede/Documents/GitHub/"
 
@@ -8,9 +10,11 @@ def Remove():
     directoryName = str(sys.argv[1])
     try:
         os.removedirs(path + directoryName)
+        g = Github(token)
+        g.get_repo(directoryName).delete()
+        print("Directory {Directory} at {path} was deleted {time}".format(Directory = directoryName, path = path, time = str(datetime.datetime.now())))
     except FileNotFoundError:
         print("This folder does not exits")
-    print("Directory {Directory} at {path} was deleted {time}".format(Directory = directoryName, path = path, time = str(datetime.datetime.now())))
 
 if __name__ == "__main__":
     Remove()
